20d5328fcf38032b7691679e5301330735e7dde7
Julian Nadeau <julian@jnadeau.ca>
2016-12-24 16:56:26 -0500

    Better logger

--- Gemfile
+++ Gemfile
@@ -4,5 +4,5 @@ gem 'capistrano', '~> 3.7'
 gem 'capistrano-bundler', '~> 1.2'
 gem 'ejson'
 gem 'byebug'
-gem 'daemons'
-gem 'remote_syslog_logger'
+gem 'daemons' # Install on host
+gem 'remote_syslog_logger' # Install on host
--- lib/scripts/helpers/sys_logger.rb
+++ lib/scripts/helpers/sys_logger.rb
@@ -2,6 +2,10 @@ require 'remote_syslog_logger'
 
 class SysLogger
   def self.logger
-    RemoteSyslogLogger.new('logs2.papertrailapp.com', 26759)
+    logger = RemoteSyslogLogger.new('logs2.papertrailapp.com', 26759)
+    logger.formatter = proc { |severity, datetime, _, msg|
+      "#{severity} [#{caller[4]}] [#{datetime}] #{msg}\n"
+    }
+    logger
   end
 end
