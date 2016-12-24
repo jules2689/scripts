d3c7854a241266631b317ed937911b23dfd238e7
Julian Nadeau <julian@jnadeau.ca>
2016-12-24 16:58:27 -0500

    Already in there

--- lib/scripts/helpers/sys_logger.rb
+++ lib/scripts/helpers/sys_logger.rb
@@ -2,10 +2,6 @@ require 'remote_syslog_logger'
 
 class SysLogger
   def self.logger
-    logger = RemoteSyslogLogger.new('logs2.papertrailapp.com', 26759)
-    logger.formatter = proc { |severity, datetime, _, msg|
-      "#{severity} [#{caller[4]}] [#{datetime}] #{msg}\n"
-    }
-    logger
+    RemoteSyslogLogger.new('logs2.papertrailapp.com', 26759)
   end
 end
--- lib/scripts/script2.rb
+++ lib/scripts/script2.rb
@@ -1,6 +1,6 @@
 require_relative 'helpers/sys_logger'
 
 loop do
-  SysLogger.logger.info "#{Time.now} -> Logging background in script2.rb"
+  SysLogger.logger.info "Logging background in script2.rb"
   sleep 1
 end
