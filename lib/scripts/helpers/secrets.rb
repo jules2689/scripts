1fd469d4c6c5e7f113b5a1ad2834c40b7cedec08
Julian Nadeau <julian@jnadeau.ca>
2016-12-25 20:23:54 -0500

    Fix secrets

--- lib/scripts/helpers/secrets.rb
+++ lib/scripts/helpers/secrets.rb
@@ -2,6 +2,7 @@ require 'json'
 
 class Secrets
   def self.secrets
-    JSON.parse(File.read("config/secrets.json"))
+    file_path = File.expand_path("../../../../config/secrets.json", __FILE__)
+    JSON.parse(File.read(file_path))
   end
 end
