a27e28367506d6750926ddc490e860f73feb44a4
Julian Nadeau <julian@jnadeau.ca>
2017-01-08 22:10:58 -0500

    Explain the parititoner better

--- lib/scripts/helpers/schedule_partitioner.rb
+++ lib/scripts/helpers/schedule_partitioner.rb
@@ -18,12 +18,19 @@ class SchedulePartitioner
     end
   end
 
+  # Simply put, this function will shove articles into the "minimum bucket"
+  # This will greedily sort the articles until such a time that all are exhausted
+  # and sorted. We let the minimum bucket function know the read time of the article
+  # to help aid the decision so we don't go over max value by very much
   def partition
     @articles.each do |article|
       minimum_bucket(article['read_time'].to_i) << article
     end
   end
 
+  # The minimum bucket is the bucket with the least value (determined by read time)
+  # There is a max value set at initialization and a max size for the entries in a bucket
+  # We can go over the max value by a little bit to help
   def minimum_bucket(value_to_add)
     candidate_buckets = @buckets.reject do |bucket|
       # Already over max bucket size
@@ -34,6 +41,8 @@ class SchedulePartitioner
       (!bucket.empty? && value_of_bucket(bucket) + value_to_add >= @max_value + DEFAULT_VALUE)
     end
 
+    # If we have no candidate buckets, we've exhausted all current buckets, so add another
+    # Candidate buckets will now be all buckets. The min_by function below will get the empty bucket
     if candidate_buckets.empty?
       @buckets << []
       candidate_buckets = @buckets
@@ -42,6 +51,8 @@ class SchedulePartitioner
     candidate_buckets.min_by { |bucket| value_of_bucket(bucket) }
   end
 
+  # The value of a bucket is the sum of all read times of articles, if an article has a 0 minute read time
+  # assume it is worth DEFAULT_VALUE
   def value_of_bucket(bucket)
     bucket.collect { |v| v["read_time"].to_i > 0 ? v['read_time'].to_i : DEFAULT_VALUE }.inject(&:+) || 0
   end
