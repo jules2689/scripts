86ebb669525c833b97553020114b1d024e8bc870
Julian Nadeau <julian@jnadeau.ca>
2017-04-30 23:53:48 -0400

    Special handling for some images

--- lib/scripts/wishlist/generic.rb
+++ lib/scripts/wishlist/generic.rb
@@ -8,17 +8,24 @@ module Wishlist
       def scrape(url)
         u_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
         page = MetaInspector.new(url, headers: { 'User-Agent' => u_agent })
-
         {
           title: page.best_title,
-          price: find_price(page.to_s),
+          price: page.meta['og:price:amount'] || find_price(page.to_s),
           desc:  page.best_description,
-          image: page.images.best,
+          image: image(url, page),
           url: url
         }
       end
 
       private
+
+      def image(url, page)
+        if url.match(/structube.com/)
+          page.to_s.match(%r{<img data-lazy=\\\"([^\s]*) })[1].gsub(%r{\\/}, '/')[0..-3]
+        else
+          page.images.best
+        end
+      end
 
       def find_price(content)
         m = content.match(PRICE_REGEX)
